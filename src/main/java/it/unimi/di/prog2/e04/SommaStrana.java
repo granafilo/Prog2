c2d2bd0830882f82f98dd4e50022faf9f1468d31
granafilo <104501051+granafilo@users.noreply.github.com>
2024-12-09 16:17:43 +0100

    n

--- src/main/java/it/unimi/di/prog2/e04/SommaStrana.java
+++ src/main/java/it/unimi/di/prog2/e04/SommaStrana.java
@@ -1,52 +1,59 @@
-/*
-
-Copyright 2024 Massimo Santini
-
-This file is part of "Programmazione 2 @ UniMI" teaching material.
-
-This is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This material is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this file.  If not, see <https://www.gnu.org/licenses/>.
-
-*/
-
 package it.unimi.di.prog2.e04;
 
-import java.util.Vector;
+import java.util.*;
 
-/**
- * Vedi <a
- * href="https://github.com/mapio/labprog/blob/master/esercizi/somma_strana/Testo.md">testo</a>, ma
- * leggendo gli addendi dal flusso di ingresso.
- */
 public class SommaStrana {
-
-  /** . */
   private SommaStrana() {}
-
-  // Per memorizzare un elenco di interi si può usare list<Integer>, grazie
-  // all'boxing automatico https://dev.java/learn/numbers-strings/autoboxing/
-  // se la lista è stata dichiarata come
-  //
-  // List<Integer> interi = new ArrayList<>();
-  //
-  // sono legittime espressioni del tipo
-  //
-  // interi.add(3);
-  // int y = interi.get(0);
-  //
-  // dove vengono messi e prelevati dalla lista degli int, non degli Integer.
-
   public static void main(String[] args) {
-  }
+    Vector<Vector<Integer>> interi = new Vector<>();
+    int max = 0, iter = 0, i = 0, j = 0, s1 = 0, s2 = 0, somma = 0, riporto = 0;
 
+    //inserisco in un vettore all'interno di un'altro vettore tutti i numeri che ricevo in input
+    try (Scanner s = new Scanner(System.in)) {
+        while (s.hasNext()) {
+            final String linea = s.nextLine();
+            ArrayList<Integer> numeri = new ArrayList<>(linea.length); 
+            for (char c : linea.toCharArray()) {
+                if (Character.isDigit(c)) {
+                    numeri.add(Character.getNumericValue(c));
+                }
+            }
+            interi.add(numeri);
+            max = Math.max(max, numeri.size());
+        }
+    }
+
+    Vector<Integer> risultati = new Vector<>(Collections.nCopies(max, 0));
+
+    // Rendo tutti i vettori di dimensione massima, aggiungendo zeri a sinistra
+
+    for (Vector<Integer> a : interi) {
+      while(a.size() < max){
+        a.add(0,0);
+      }
+    }
+
+    //eseguo la somma
+    for(i = 0; i < interi.size(); i++){
+      for(j = max-1; j>=0; j--){
+        s1 = interi.get(i).get(j);
+        s2 = risultati.get(j);
+        somma = s1+s2+riporto;
+        if(somma > 9){
+          riporto = 1;
+          somma = 9 - (somma % 10);
+        }else{
+          riporto = 0;
+        }
+        risultati.set(j, somma);
+      }
+      if((i == interi.size()-1) && riporto == 1) risultati.add(0,1);
+    }
+
+    //stampo il risultato
+    for(i = 0; i<risultati.size(); i++){
+      System.out.print(risultati.get(i));
+    }
+
+  }
 }
